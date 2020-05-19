0cabbf8ff921fdc843eff439fadf885a6d5596a1
Ramon Audain <raudain123@gmail.com>
2020-05-19 01:34:19 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Given a string s, find the longest palindromic substring in a s.s
+// Example:
+// longestPalindrome("cbbd") --> "bb"
+// longestPalindrome("abba") --> "abba"
+// longestPalindrome("a") --> "a"
+
+
+function longestPalindrome(s){
+  
+}
