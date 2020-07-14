a0e9424b01d7dedc00b1c556dbfecb184e64a63b
Ramon Audain <raudain123@gmail.com>
2020-07-14 01:34:06 +0000

    solution

--- main.js
+++ main.js
@@ -4,7 +4,47 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
+// Technique: Expand around the center
 
 function longestPalindrome(s){
-  
+    let startIndex = 0;
+    let maxLength = 1; // Length can't be less than 1
+    
+    function expandAroundMiddle(left, right){
+        while(left >= 0 && right < s.length && s[left] === s[right]){
+            const currentLength = right - left + 1;
+            
+            // Used in the slice method for return
+            if (currentLength > maxLength){
+                maxLength = currentLength;
+                startIndex = left;
+            }
+            
+            console.log(left, right)
+            
+            left--;
+            right++;
+        }
+    }
+    
+    
+    for (let i = 0; i < s.length; i++){
+        
+        // For odd length, checks characters to the left and right
+        expandAroundMiddle(i-1, i+1)
+        
+        // For even length, checks characters
+        expandAroundMiddle(i, i+1)
+
+        console.log(s.slice(startIndex, startIndex+maxLength))
+    }
+    
+    console.log("start", startIndex, "max", maxLength)
+    
+    return s.slice(startIndex, startIndex+maxLength);
 }
+
+
+// longestPalindrome("cbbd");
+// longestPalindrome("abba");
+longestPalindrome("asadljfslajfsaljfl;kk;kkjjjjjhhhhggbajfsaljfdsaljfddrpotfsrsa");
